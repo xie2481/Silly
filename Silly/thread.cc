25f7c2b0c3f576c5d10f79dd349f7e2193971069
xie2481 <2481086105@qq.com>
2020-01-30 21:07:49 -0800

    添加了互斥锁，信号量，读写锁

--- Silly/thread.cc
+++ Silly/thread.cc
@@ -6,17 +6,101 @@ namespace Silly{
 
 static thread_local Thread * currentThread = nullptr;  
 
+Semaphore::Semaphore(uint32_t count){
+    if(::sem_init(&m_semaphore,0,count)){
+        SILLY_LOG_ERROR("system") << "Init Semaphore error";
+        throw std::logic_error("Init Semaphore error");
+    }
+}
+
+Semaphore::~Semaphore(){
+    if(::sem_destroy(&m_semaphore)){ 
+        SILLY_LOG_ERROR("system") << "Destroy Semaphore error";
+    } 
+}
+
+void Semaphore::wait(){
+    if(::sem_wait(&m_semaphore)){
+        SILLY_LOG_ERROR("system") << "Semaphore wait error";
+        throw std::logic_error("Semaphore wait error");
+    } 
+}
+
+void Semaphore::notify(){
+   if(::sem_post(&m_semaphore) == -1){
+       SILLY_LOG_ERROR("system") << "Notify error";
+       throw std::logic_error("Notify error");
+   } 
+}
+
+Mutex::Mutex(){
+    if(::pthread_mutex_init(&m_mutex,nullptr)){
+        SILLY_LOG_ERROR("system") << "Mutex init error";
+        throw std::logic_error("Mutex init error");
+    }
+}
+
+Mutex::~Mutex(){
+    ::pthread_mutex_destroy(&m_mutex);
+}
+
+void Mutex::lock(){
+   if(::pthread_mutex_lock(&m_mutex)){
+       SILLY_LOG_ERROR("system") << "Mutex lock error";
+       throw std::logic_error("Mutex lock error");
+   } 
+}
+
+void Mutex::unlock(){
+    if(::pthread_mutex_unlock(&m_mutex)){
+       SILLY_LOG_ERROR("system") << "Mutex unlock error";
+       throw std::logic_error("Mutex unlock error");
+    }
+}
+
+RWMutex::RWMutex(){
+    if(::pthread_rwlock_init(&m_mutex,nullptr)){
+        SILLY_LOG_ERROR("system") << "RWMutex init error";
+        throw std::logic_error("RWMutex init error");
+    }
+}
+
+RWMutex::~RWMutex(){
+    ::pthread_rwlock_destroy(&m_mutex);
+}
+
+void RWMutex::rdlock(){
+    if(::pthread_rwlock_rdlock(&m_mutex)){
+        SILLY_LOG_ERROR("system") << "RWMutex read lock error";
+        throw std::logic_error("RWMutex read lock error");
+    }
+}
+
+void RWMutex::wrlock(){
+    if(::pthread_rwlock_wrlock(&m_mutex)){
+        SILLY_LOG_ERROR("system") << "RWMutex write lock error";
+        throw std::logic_error("RWMutex write lock error");
+    }
+}
+
+void RWMutex::unlock(){
+    if(::pthread_rwlock_unlock(&m_mutex)){
+        SILLY_LOG_ERROR("system") << "RWMutex unlock error";
+        throw std::logic_error("RWMutex unlock error");
+    }
+}
+
 Thread::Thread(const std::function<void()> & cb,const std::string & name)
 :m_cb(cb),
- m_name(name)
+ m_name(name),
+ m_semaphore(0)
 {
     int ret = ::pthread_create(&m_thread,NULL,Thread::run,(void*)this);
     if(ret){
        SILLY_LOG_ERROR("system") << "Thread " << name << " create failure"; 
        throw std::logic_error("Thread " + name + " create failure");
     } 
-    currentThread = this;
-    pthread_setname_np(m_thread,m_name.substr(0,15).c_str());     
+    m_semaphore.wait();
 }
 
 void Thread::join(){
@@ -49,8 +133,11 @@ std::string Thread::GetName(){
 
 void * Thread::run(void * arg){
     Thread * newThread = static_cast<Thread*>(arg);
+    currentThread = newThread;
+    pthread_setname_np(newThread->m_thread,newThread->m_name.substr(0,15).c_str());     
     std::function<void()> cb;
     cb.swap(newThread->m_cb);
+    newThread->m_semaphore.notify();
     cb();
     return nullptr;
 }
--- Silly/thread.h
+++ Silly/thread.h
@@ -6,7 +6,154 @@
 #include <pthread.h>
 #include <functional>
 #include <string>
+#include <semaphore.h>
 namespace Silly{
+
+    class Semaphore
+    {
+        public:
+            Semaphore(uint32_t count);
+            ~Semaphore();
+
+            void wait();
+            void notify();
+
+        private:
+            Semaphore(const Semaphore &) = delete;
+            Semaphore(const Semaphore &&) = delete;
+            Semaphore & operator=(const Semaphore &) = delete;
+        
+        private:
+            sem_t m_semaphore;
+    };
+
+    template<class T>
+    class ScopeMutexLock
+    {
+        public:
+          ScopeMutexLock(T & mutex)
+          :m_mutex(mutex),
+           m_locked(false){
+            lock();
+          }
+
+          ~ScopeMutexLock(){
+            unlock();
+          }
+           
+          void lock(){
+            if(!m_locked){
+                m_mutex.lock();
+                m_locked = true;
+            }
+          }
+          void unlock(){
+            if(m_locked){
+                m_mutex.unlock();
+                m_locked = false;
+            }
+          } 
+        private:
+            T & m_mutex;
+            bool m_locked;
+    };
+
+    class Mutex{
+        public:
+            typedef ScopeMutexLock<Mutex> MutexLock;
+            Mutex();
+            ~Mutex();
+
+            void lock();
+            void unlock();
+
+        private:
+            Mutex(const Mutex &) =delete;
+            Mutex(const Mutex &&) = delete;
+            Mutex & operator=(const Mutex &) = delete;
+    
+        private:
+            pthread_mutex_t m_mutex;
+    };
+
+    template<class T>
+    class ReadScopeMutexLock
+    {
+        public:
+          ReadScopeMutexLock(T & mutex)
+          :m_mutex(mutex),
+           m_locked(false){
+            lock();
+          }
+
+          ~ReadScopeMutexLock(){
+            unlock();
+          }
+           
+          void lock(){
+            if(!m_locked){
+                m_mutex.rdlock();
+                m_locked = true;
+            }
+          }
+          void unlock(){
+            if(m_locked){
+                m_mutex.unlock();
+                m_locked = false;
+            }
+          } 
+        private:
+            T & m_mutex;
+            bool m_locked;
+    };
+
+    template<class T>
+    class WriteScopeMutexLock
+    {
+        public:
+          WriteScopeMutexLock(T & mutex)
+          :m_mutex(mutex),
+           m_locked(false){
+            lock();
+          }
+
+          ~WriteScopeMutexLock(){
+            unlock();
+          }
+           
+          void lock(){
+            if(!m_locked){
+                m_mutex.wrlock();
+                m_locked = true;
+            }
+          }
+          void unlock(){
+            if(m_locked){
+                m_mutex.unlock();
+                m_locked = false;
+            }
+          } 
+        private:
+            T & m_mutex;
+            bool m_locked;
+    };
+   
+    class RWMutex
+    {
+        public:
+            typedef ReadScopeMutexLock<RWMutex> ReadLock;
+            typedef WriteScopeMutexLock<RWMutex> WriteLock;
+            RWMutex();
+            ~RWMutex();
+
+            void rdlock();
+            void wrlock();
+            void unlock();
+
+        private:
+          pthread_rwlock_t m_mutex;
+    }; 
+
     class Thread
     {
         public:
@@ -29,6 +176,7 @@ namespace Silly{
             std::function<void()> m_cb;
             pthread_t m_thread;
             std::string m_name;
+            Semaphore m_semaphore;
     };
     
 } // end of namespace
